2e88705b47fdc06e02845086fe14ef0cb0539049
KsquaredK <ksquaredkelly@gmail.com>
2022-01-18 10:54:09 -0600

    fix CLI UI in projects, add package-lock.json

--- CodeAcademy-projects.js
+++ CodeAcademy-projects.js
@@ -13,7 +13,7 @@ newton = Math.floor(newton)
 
 // Math.floor() rounds down decimals
 fahrenheit = Math.floor(fahrenheit)
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit, ${newton} degrees Newton, ${celsius} degrees Celsius, and ${kelvin} degrees Kelvin.`)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit, ${newton} degrees Newton, ${celsius} degrees Celsius, and ${kelvin} degrees Kelvin.`, "\n")
 
 /*  DOG YEARS */
 // declare variable, assign value of current age
@@ -33,7 +33,7 @@ let myAgeInDogYears = earlyYears + laterYears
 let myName= "Kelly"
 // console.log(myName.toLowerCase())
 
-console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`)
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`, "\n")
 
 /* MAGIC 8-BALL - switch case  */
 let userName = 'Prue'
@@ -77,7 +77,7 @@ else {
    default: console.log(`I dunno...`)
 
 }
-   console.log(eightBall)
+   console.log(eightBall, "\n")
 
    /*  Magic 8-Ball - else if  */
 //    let userName = 'Prue'
@@ -134,30 +134,30 @@ else {
 
   // randomly assigns a number < 1000
 let raceNumber = Math.floor(Math.random() * 1000);
+let runnerAge = Math.floor(Math.random() * 102);
 
 // assign variables for runners' ages and race times
 let earlyRegistrant = true;
-let runnerAge = 18;
 
 // generate random race numbers > 1000 for early race registrants
 if (earlyRegistrant == true && runnerAge > 18) {
   raceNumber = raceNumber + 1000;
 
 // Log race time and race number for early registrants
-  console.log(`Your race number is ${raceNumber}, and your start time is 9:30 am.`)
+  console.log(`Your race number is ${raceNumber}, and your start time is 9:30 am.`, "\n")
 
 // 
 } else if
 (earlyRegistrant !== true && runnerAge > 18) {
-  console.log(`Your race number is ${raceNumber} and your start time is 11 am.`)
+  console.log(`Your race number is ${raceNumber} and your start time is 11 am.`, "\n")
 }
 
 if (runnerAge < 18) {
-  console.log(`Your race number is ${raceNumber} and your start time 12:30 pm.`)
+  console.log(`Your race number is ${raceNumber} and your start time 12:30 pm.`, "\n")
 }
 
 if (runnerAge == 18) {
-  console.log(`See registration desk.`)
+  console.log(`See registration desk.`, "\n")
 }
 
 //  ====*** ROCK SCISSORS PAPER GAME featuring cheatcode option
@@ -165,10 +165,7 @@ const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
     if (userInput === "rock" || userInput === "paper" || userInput === "scissors" || userInput ==="bomb") {
           return userInput;
-    } else {
-    console.log("You just had one job.","\n","\n","One job and three little old words: rock, paper, or scissors.", "\n","\n", "Try again, genius.", "\n");
-    }
-  
+    } 
   }
   
   
@@ -193,44 +190,47 @@ const getUserChoice = userInput => {
   
   const determineWinner = (userChoice, computerChoice) => {
     if (userChoice === "bomb") {
-    return `YOU WIN!!! YOU WIN!!! OMFG!!!`;
+    return `YOU WIN!!! YOU WIN!!! OMFG!!!", \n"`;
       }
     if (userChoice === computerChoice) {
-    return `Tie game! Everybody wins!`;
+    return `Tie game! Everybody wins!`, "\n";
     }
     if (userChoice === "rock") {
       if (computerChoice === "paper") {
-      return `Rock? I don't see any rock...YOU LOSE!`;
+      return `Rock? I don't see any rock...YOU LOSE!`, "\n";
       }
       else {
-      return `Smashy-smash! Rock wins!!`;
+      return `Smashy-smash! Rock wins!!`, "\n";
     }
     }
   
     if (userChoice === "paper") {
       if (computerChoice === "scissors") {
-        return `Ouchy, you got shredded...YOU LOSE!`;
+        return `Ouchy, you got shredded...YOU LOSE!`, "\n";
       }
       else {
-        return `Buh-BYE, rock. Paper wins!!`;
+        return `Buh-BYE, rock. Paper wins!!`, "\n";
       }
     }
   
     if (userChoice === "scissors") {
       if (computerChoice === "rock") {
-        return `Beware of falling boulders, Mr. Snippy...YOU LOSE!`;
+        return `Beware of falling boulders, Mr. Snippy...YOU LOSE!`, "\n";
       }
       else {
-        return `Snip. Snip. Snip...Scissors win!!`
+        return `Snip. Snip. Snip...Scissors win!!`, "\n";
       }
     }
+    else {
+      return console.log(" You just had one job.","\n","One job and three little old words to choose from: rock, paper, or scissors.", "\n","Try again, genius.", "\n");
+      }
   }
   
   const playGame = () => {
-    const userChoice = getUserChoice("rock");
+    const userChoice = getUserChoice("bip");
     console.log(`You threw: ${userChoice}`);
     const computerChoice = getComputerChoice();
-    console.log(`The computer threw: ${computerChoice}.`, "\n","\n", "And the winner is: ", "\n");
+    console.log(`The computer threw: ${computerChoice}`,"\n","And the winner is: ", "\n");
     console.log(determineWinner(userChoice, computerChoice));
     };
     
